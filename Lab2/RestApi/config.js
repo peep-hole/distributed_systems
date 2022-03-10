26487c79ad63d516a7ee8b909203e033f3e8d825
Maciej Kuś <skyVIP1234@gmail.com>
2022-03-10 17:39:40 +0100

    Update config.js

--- Lab2/RestApi/config.js
+++ Lab2/RestApi/config.js
@@ -1,7 +1,7 @@
 const constants = {
     api1: {
         BASE_URL: "https://newsapi.org/v2/everything?q=",
-        SECRET_KEY: "26fddf17dcb14c7f8e8726fec334855d"
+        SECRET_KEY: "insert_key"
     },
     api2 : {
         BASE_URL: "https://thecocktaildb.com/api/json/v1/1/search.php?s=",
